32eeb1658a54e71cc046772a5b9297e67cdf9086
Niobisoft <niobisoft@gmail.com>
2018-10-12 12:29:42 +0200

    after master failers

--- formIO/formIO.php
+++ formIO/formIO.php
@@ -10,4 +10,51 @@ echo "ahoj";
 function formInit($action, $method, $optional) {
   echo "<form method=\"".$method."\" action=\"".$action."\" ".$optional.">";
 }
+
+//
+//
+
+
+function formSelect ($type, $name, $text, $optional) {
+    if ($type == "select") {
+      echo "<select name=\"".$name."\" ".$optional.">";
+    }
+    if ($type == "option") {
+      echo "<option value=\"".$name."\" ".$optional.">".$text
+      closer("option");
+    }
+    if ($type == "datalist") {
+      echo "<datalist id=\"".$name."\">"
+    }
+}
+function formTextarea  ($name, $text, $optional) {
+  echo "<textarea name=\"".$name."\" ".$optional.">".$text;
+  closer("textarea");
+}
+function formButton ($type, $form, $value, $text, $optional) {
+  echo "<button type=\"".$type."\" form=\""$form"\" value=\".$value.\" ".$optional.">".$text
+  closer("button");
+}
+
+
+
+//
+// nezapomenout na input list="" (formSelect.datalist) identifikator name
+//
+
+//function formInput ($type, $name, ) {
+//
+//}
+
+//
+// Support fcs
+//
+function formStarter ($name) {
+    echo "<".$name.">";
+}
+
+function formCloser ($name) {
+    echo "</".$name.">";
+}
+
 ?>
